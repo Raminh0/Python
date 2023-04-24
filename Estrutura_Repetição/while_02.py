1f0a846bb2c927cca3dcc656448dca9c5ca13af1
Inajá Ramos <112290663+Raminh0@users.noreply.github.com>
2023-04-24 14:28:17 -0300

    Update while_02.py

--- Estrutura_Repetição/while_02.py
+++ Estrutura_Repetição/while_02.py
@@ -2,9 +2,12 @@ senha= input("Cadastro:\n\nCrie uma senha: ")
 print("Senha caddastrada com sucesso!")
 entrar=input("\n\n\nEntrar\n Informe sua senha: ")
 cont= 2
-while (senha != entrar) and (cont != 0): #and (novamente != senha):
+while (senha != entrar) and (cont != 0):
+    print(cont," Chance(s) para tentar novamente.")
+    entrar= input("**Senha incorreta!\n---Tente novamente ou redefina sua senha.---\n\n")
     cont-=1
-    print("Chance:",cont," para tentar novamente.")
-    entrar= input("Senha incorreta!\n\nTente novamente.\n\n")
     break
-print("Você conseguiu entrar CARAIOOOOOOOOOOO!")
+if (senha == entrar):    
+    print("Você conseguiu entrar! Bem vindo(a)!")
+else:
+    print("O número de tentativas de acesso foi atingido. É necessário redefinir sua senha!")
